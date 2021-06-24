8b466cd3de7f78e89cca6090dba15c545ccd07e3
Pavel Shell <pasha.technician@bk.ru>
2021-06-24 19:41:45 +0300

    Добавьте файлы проекта.

---
+++ 22 (FunctionGraphBuilder)/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace _22__FunctionGraphBuilder_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.operations = new System.Windows.Forms.ComboBox();
+            this.value = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 44);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Black;
+            series1.Name = "Series2";
+            series2.BorderWidth = 5;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Black;
+            series2.Name = "Series3";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Name = "Series1";
+            series3.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(514, 338);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "y = x";
+            // 
+            // operations
+            // 
+            this.operations.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.operations.FormattingEnabled = true;
+            this.operations.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/",
+            "^"});
+            this.operations.Location = new System.Drawing.Point(62, 14);
+            this.operations.Name = "operations";
+            this.operations.Size = new System.Drawing.Size(52, 24);
+            this.operations.TabIndex = 2;
+            this.operations.SelectedIndexChanged += new System.EventHandler(this.operations_SelectedIndexChanged);
+            // 
+            // value
+            // 
+            this.value.Location = new System.Drawing.Point(120, 17);
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(100, 22);
+            this.value.TabIndex = 3;
+            this.value.Text = "0";
+            this.value.TextChanged += new System.EventHandler(this.value_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 399);
+            this.Controls.Add(this.value);
+            this.Controls.Add(this.operations);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.MinimumSize = new System.Drawing.Size(260, 310);
+            this.Name = "Form1";
+            this.Text = "Графики";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox operations;
+        private System.Windows.Forms.TextBox value;
+    }
+}
+
---
+++ 22 (FunctionGraphBuilder)/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22__FunctionGraphBuilder_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void value_TextChanged(object sender, EventArgs e)
+        {
+            if (operations.Text != "" && value.Text != "")
+            {
+                createGraph();
+            }
+        }
+
+        private void operations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(operations.Text != "" && value.Text != "")
+            {
+                createGraph();
+            }
+        }
+
+        private void createGraph()
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+
+            for (int x = -10; x <= 10; x++)
+            {
+                Single y = F(operations.Text, x.ToString(), value.Text);
+
+                chart1.Series[0].Points.AddXY(x, 0);
+                chart1.Series[1].Points.AddXY(0, y);
+
+                chart1.Series[2].Points.AddXY(x, y);
+            }
+        }
+
+        private Single F(String operations, String valueX, String valueB)
+        {
+            Single x = Convert.ToSingle(valueX);
+            Single b = Convert.ToSingle(valueB);
+
+            switch (operations)
+            {
+                case "+":
+                    chart1.Series[2].Color = Color.Red;
+                    return x + b;
+                case "-":
+                    chart1.Series[2].Color = Color.Blue;
+                    return x - b;
+                case "*":
+                    chart1.Series[2].Color = Color.Green;
+                    return x * b;
+                case "/":
+                    chart1.Series[2].Color = Color.Brown;
+                    return x / b;
+                default:
+                    chart1.Series[2].Color = Color.BurlyWood;
+                    return (float)Math.Pow(x, b);
+            }
+        }
+    }
+}
